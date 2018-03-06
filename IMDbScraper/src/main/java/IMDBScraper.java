3657892d4db1c41618c30841e27ed59ea5e5bb53
Anu Tomy <anuatomy@gmail.com>
2018-03-06 13:50:45 -0500

    Scrape the top 250 movie names and ratings from IMDb

--- IMDbScraper/src/main/java/IMDBScraper.java
+++ IMDbScraper/src/main/java/IMDBScraper.java
@@ -1,12 +1,19 @@
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 
 public class IMDbScraper {
     public static void main(String[] args)throws Exception {
 
-        final org.jsoup.nodes.Document document = Jsoup.connect("http://www.imdb.com").get();
-        //Print html of page
-        System.out.println(document.outerHtml());
+        final org.jsoup.nodes.Document document = Jsoup.connect("http://www.imdb.com/chart/top").get();
+         //Print html of page
+        //System.out.println(document.outerHtml());
 
+        for(Element row : document.select("table.chart.full-width tr")){
+            final String title = row.select(".titleColumn a").text();
+            final String rating = row.select(".imdbRating").text();
+
+            System.out.println(title + " --> Rating: "+ rating);
+        }
     }
 }
 
