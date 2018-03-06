9ea3b4f693d4f73e72a2db959c4173bdfbb17144
Anu Tomy <anuatomy@gmail.com>
2018-03-06 11:18:13 -0500

    Initial Commit

--- IMDbScraper/src/main/java/IMDBScraper.java
+++ IMDbScraper/src/main/java/IMDBScraper.java
@@ -1,12 +1,16 @@
 import org.jsoup.Jsoup;
 
-public class IMDBScraper {
+public class IMDbScraper {
     public static void main(String[] args)throws Exception {
+
         final org.jsoup.nodes.Document document = Jsoup.connect("http://www.imdb.com").get();
         //Print html of page
         System.out.println(document.outerHtml());
 
     }
-
-
 }
+
+
+
+
+
